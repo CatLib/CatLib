b61c5225d178093293ba21ce16afdd629d402236
喵喵大人 <support@catlib.io>
2018-12-18 15:39:50 +0800

    rename copyright

--- CatLib/Bootstrap/BootstrapTypeFinder.cs
+++ CatLib/Bootstrap/BootstrapTypeFinder.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * This file is part of the CatLib package.
  *
- * (c) Yu Bin <support@catlib.io>
+ * (c) CatLib <support@catlib.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
