b0a10b679f7cac5acb4ba4e1bbe440f27a3d21d3
喵喵大人 <support@catlib.io>
2019-01-04 17:54:58 +0800

    sync core

--- Game/Main.cs
+++ Game/Main.cs
@@ -37,34 +37,6 @@ namespace Game
         }
 
         /// <summary>
-        /// 当服务提供者初始化之前
-        /// </summary>
-        /// <param name="provider">准备初始化的服务提供者</param>
-        protected override void OnProviderInit(IServiceProvider provider)
-        {
-            base.OnProviderInit(provider);
-            Debug.Log("Initing Provider [<color=#00ff00>" + provider + "</color>]");
-        }
-
-        /// <summary>
-        /// 当框架终止之前
-        /// </summary>
-        protected override void OnTerminate()
-        {
-            base.OnTerminate();
-            Debug.Log("<color=#00ffff>OnTerminate</color>");
-        }
-
-        /// <summary>
-        /// 当框架终止之后
-        /// </summary>
-        protected override void OnTerminated()
-        {
-            base.OnTerminated();
-            Debug.Log("<color=#ff0000>OnTerminated</color>");
-        }
-
-        /// <summary>
         /// 获取引导程序
         /// </summary>
         /// <returns>引导脚本</returns>
