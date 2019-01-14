aea8ad0418c199d8cd9309aa2f85e6305150c4dd
喵喵大人 <support@catlib.io>
2019-01-14 17:00:43 +0800

    resolved #12

---
+++ Game/DemoApplication.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) CatLib <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib;
+using UnityEngine;
+
+namespace Demo
+{
+    public class DemoApplication : UnityApplication
+    {
+        public DemoApplication(MonoBehaviour behaviour)
+            : base(behaviour)
+        {
+
+        }
+
+        public IBindData GetBindDataWithInstance(object instance)
+        {
+            return GetBindFillable(GetServiceWithInstanceObject(instance));
+        }
+    }
+
+    public static class ExtendDemoApplication
+    {
+        public static void Extend<TConcrete>(this IContainer container, Func<IBindData, TConcrete, object> closure)
+        {
+            container.Extend(null, (instance, _) =>
+            {
+                if (!(instance is TConcrete))
+                {
+                    return instance;
+                }
+
+                if (App.Handler is DemoApplication)
+                {
+                    var application = (DemoApplication)App.Handler;
+                    return closure(application.GetBindDataWithInstance(instance), (TConcrete)instance);
+                }
+
+                return closure(null, (TConcrete)instance);
+            });
+        }
+    }
+
+}
